424f6eb4975f5158290047d7311553b2e49e561e
Tem-news <virtuve2000@inbox.lv>
2026-08-14 00:33:43 +0300

    Add Home component with welcome message

---
+++ page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1 style={{ color: '#16a34a' }}>Sistēma ir savienota!</h1>
+      <p>Tem-Auto Tirgus veiksmīgi palaists no jauna un darbojas.</p>
+    </main>
+  )
+}
