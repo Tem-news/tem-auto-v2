14e3f96e4ced92bea34ca11e91b5ef8f17ef7947
Tem-news <virtuve2000@inbox.lv>
2026-08-17 00:50:53 +0300

    Update import path for supabase module

--- app/auto/[id]/page.tsx
+++ app/auto/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
 import Link from 'next/link'
-import { supabase } from '../../lib/supabase'
+import { supabase } from '@/lib/supabase'
 export default function AutoLapa() {
   const params = useParams()
   const id = params?.id
